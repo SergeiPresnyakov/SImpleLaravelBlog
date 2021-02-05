46e696c56779e85969de7fb92f20734857b7144d
Sergei Presnyakov <jugg010203@gmail.com>
2021-02-05 20:45:24 +0800

    posts index page

--- app/Repositories/BlogPostRepository.php
+++ app/Repositories/BlogPostRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\BlogPost as Model;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 /**
  * Class BlogPostRepository
@@ -17,5 +18,32 @@ class BlogPostRepository extends CoreRepository
     protected function getModelClass()
     {
         return Model::class;
+    }
+
+    /**
+     * Получить список статей для вывода в списке
+     * (Админка)
+     * 
+     * @return LengthAwarePaginator
+     */
+    public function getAllWithPaginate()
+    {
+        $columns = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id'
+        ];
+
+        $result = $this
+            ->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC')
+            ->paginate(20);
+        
+        return $result;
     }
 }
--- resources/views/blog/admin/posts/index.blade.php
+++ resources/views/blog/admin/posts/index.blade.php
@@ -1,5 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-    Posts index page
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+                <a href="{{ route('blog.admin.posts.create') }}" class="btn btn-primary">Write</a>
+            </nav>
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Author</th>
+                                <th>Category</th>
+                                <th>Header</th>
+                                <th>Published at</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($paginator as $post)
+                                @php
+                                    /** @var App\Models\BlogPost $post */
+                                @endphp
+                                <tr @if(!$post->is_published) style="background-color: #ccc;" @endif>
+                                    <td>{{ $post->id }}</td>
+                                    <td>{{ $post->user_id }}</td>
+                                    <td>{{ $post->category_id }}</td>
+                                    <td>
+                                        <a href="{{ route('blog.admin.posts.edit', $post->id) }}">{{ $post->title }}</a>
+                                    </td>
+                                    <td>{{ $post->published_at ? \Carbon\Carbon::parse($post->published_at)->format('d.M  H:i') : '' }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot></tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    @if ($paginator->total() > $paginator->count())
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    {{ $paginator->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+</div>
 @endsection
